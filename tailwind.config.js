0d7dd74372594c8076dde2bf61202b5c26513e29
Gaurav Singh <allstargaurav@gmail.com>
2021-12-28 15:10:52 +0530

    Add Inter Font

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,15 @@
+const defaultTheme = require('tailwindcss/defaultTheme')
+
 module.exports = {
-  mode: 'jit',
-  purge: ['./pages/**/*.{js,ts,jsx,tsx}', './components/**/*.{js,ts,jsx,tsx}'],
-  darkMode: false, // or 'media' or 'class'
+  content: [
+    "./pages/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
+  ],
   theme: {
     extend: {
+      fontFamily: {
+        sans: ['Inter', ...defaultTheme.fontFamily.sans]
+      },
       colors: {
         aex: {
           100: '#504D5F',
