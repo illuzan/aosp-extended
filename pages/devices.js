86b91b8eb785a19a26eb2921873b163a2d08ba87
Gaurav Singh <allstargaurav@gmail.com>
2022-01-13 18:52:13 +0530

    Cleanup devices page

--- pages/devices.js
+++ pages/devices.js
@@ -1,7 +1,8 @@
-import React, { Fragment, useState } from 'react'
+import React, { Fragment, useState, useEffect } from 'react'
 import { Disclosure, Transition, RadioGroup, Listbox } from '@headlessui/react'
-import { SelectorIcon } from '@heroicons/react/solid'
+import { SelectorIcon, ChevronUpIcon } from '@heroicons/react/outline'
 import Builds from '../components/Builds'
+import { useRouter } from 'next/router'
 
 function getBrands(data) {
   let set = new Set()
@@ -11,72 +12,51 @@ function getBrands(data) {
   return [...set]
 }
 
-export default function Devices({ brands, devices, initalDevices }) {
-  // let [plan, setPlan] = useState('startup')
-  // console.log(allSupportedDevices, 'allSupportedDevices')
-  // const [devices, setDevices] = useState([])
-  // const [brands, setBrands] = useState([])
-  // const [specific, setSpecific] = useState(initalDevices)
-  const [isLoading, setLoading] = useState(false)
-  const [allSupportedBrand, setAllSupportedBrand] = useState(brands)
-  const [allSupportedDevice, setAllSupportedDevice] = useState(devices)
-  const [brandSpecificDevice, setBrandSpecificDevice] = useState(initalDevices)
-  const [selectedBrand, setSelectedBrand] = useState(brands[0])
 
-  function brandSpecificDevices(deviceBrand) {
-    const test = devices.filter((device) => device.brand === deviceBrand)
-    setSpecific(test)
-  }
+export default function Devices({
+  allSupportedBrand,
+  allSupportedDevice,
+  initalDevices,
+}) {
+  const router = useRouter()
+  // const devicesQuery = filterDevices()
+  const [brandSpecificDevice, setBrandSpecificDevice] = useState(initalDevices)
+  const [selectedBrand, setSelectedBrand] = useState(allSupportedBrand[0])
+  // console.log(router.query)
 
   function filterDevices(brandName) {
-    const filteredList = allSupportedDevice.filter((device) => device.brand === brandName)
+    const filteredList = allSupportedDevice.filter(
+      (device) => device.brand === brandName
+    )
     setSelectedBrand(brandName)
     setBrandSpecificDevice(filteredList)
   }
-
-  function getBrand(data) {
-    var set = new Set()
-    data.map((device) => {
-      set.add(device.brand)
-    })
-    setBrands([...set])
-  }
-  function hello(device) {
-    console.log(device)
-  }
-  function getDeviceBuild(deviceName, androidVersion) {
-    console.log(deviceName, androidVersion)
-  }
-
-  // useEffect(() => {
-  //   async function getDevices() {
-  //     setLoading(true)
-  //     const response = await fetch('https://api.aospextended.com/devices')
-  //     const data = await response.json()
-  //     setDevices(data)
-  //     getBrand(data)
-  //     // brandSpecificDevices('Asus')
-  //     setLoading(false)
-  //     console.log(specific)
-  //   }
-  //   getDevices()
-  // }, [])
+  useEffect(() => {
+    console.log(router.query)
+    if (router.query.codeName) {
+      console.log('dssdaf')
+    }
+    // return () => {
+    //   cleanup
+    // }
+  }, [])
   return (
-    <div className='grid grid-cols-4'>
+    <div className='grid grid-cols-5'>
       {/* Brand Button */}
       <div className='hidden col-span-1 md:block'>
-        <RadioGroup className='flex flex-col' value={selectedBrand} onChange={(event) => filterDevices(event)}>
-          <RadioGroup.Label className='sr-only'>Plan</RadioGroup.Label>
+        <RadioGroup
+          className='flex flex-col'
+          value={selectedBrand}
+          onChange={(event) => filterDevices(event)}
+        >
+          <RadioGroup.Label className='sr-only'>Brands</RadioGroup.Label>
           {allSupportedBrand.map((brand, index) => (
             <RadioGroup.Option
               key={index}
               value={brand}
               className={({ checked }) =>
-                `${checked
-                  ? 'ring-2  ring-aex-accent'
-                  : ''
-                }
-                    p-4 m-2 text-left text-lg  font-medium rounded-lg text-gray-100  bg-aex-300 flex focus:outline-none cursor-pointer `
+                `${checked ? 'ring-2  ring-aex-accent' : ''}
+                    p-4 m-2 text-left font-medium rounded-lg text-gray-100  bg-aex-300 flex focus:outline-none cursor-pointer hover:bg-aex-200 transition-all `
               }
             >
               {brand}
@@ -85,15 +65,18 @@ export default function Devices({ brands, devices, initalDevices }) {
         </RadioGroup>
       </div>
       {/* Brand Listbox */}
-      <div className="col-span-full md:hidden">
-        <Listbox value={selectedBrand} onChange={(event) => filterDevices(event)}>
+      <div className='col-span-full md:hidden'>
+        <Listbox
+          value={selectedBrand}
+          onChange={(event) => filterDevices(event)}
+        >
           <Listbox.Label className='sr-only'>Brands</Listbox.Label>
-          <div className='mt-1 relative'>
-            <Listbox.Button className='bg-aex-300 text-gray-100 relative w-full h-12 border border-aex-300 rounded-md shadow-2xl pl-3 pr-10 py-2 text-left cursor-default focus:outline-none focus:ring-1 focus:ring-aex-accent focus:border-aex-accent sm:text-sm'>
-              <span className='block truncate '>{selectedBrand}</span>
+          <div className='relative mt-1'>
+            <Listbox.Button className='relative w-full h-12 py-2 pl-4 pr-10 font-medium text-left text-gray-100 border rounded-md shadow-2xl cursor-pointer bg-aex-300 border-aex-300 focus:outline-none focus:ring-1 focus:ring-aex-accent focus:border-aex-accent'>
+              <span className='block truncate'>{selectedBrand}</span>
               <span className='absolute inset-y-0 right-0 flex items-center pr-2 pointer-events-none'>
                 <SelectorIcon
-                  className='h-5 w-5 text-gray-100'
+                  className='w-5 h-5 text-gray-100'
                   aria-hidden='true'
                 />
               </span>
@@ -105,7 +88,7 @@ export default function Devices({ brands, devices, initalDevices }) {
               leaveFrom='opacity-100'
               leaveTo='opacity-0'
             >
-              <Listbox.Options className='absolute z-10 mt-1 w-full bg-aex-300 shadow-lg max-h-60 rounded-md py-1 text-base ring-1 ring-black ring-opacity-5 overflow-auto focus:outline-none sm:text-sm'>
+              <Listbox.Options className='absolute z-10 w-full py-1 mt-1 overflow-auto text-base rounded-md shadow-lg bg-aex-300 max-h-60 ring-1 ring-black ring-opacity-5 focus:outline-none sm:text-sm'>
                 {allSupportedBrand.map((brand, index) => (
                   <Listbox.Option
                     key={index}
@@ -116,10 +99,7 @@ export default function Devices({ brands, devices, initalDevices }) {
                     }
                     value={brand}
                   >
-                    <span className='block truncate'
-                    >
-                      {brand}
-                    </span>
+                    <span className='block truncate'>{brand}</span>
                   </Listbox.Option>
                 ))}
               </Listbox.Options>
@@ -129,66 +109,49 @@ export default function Devices({ brands, devices, initalDevices }) {
       </div>
 
       <h1 className='col-span-full md:col-start-2'>
-        {isLoading ? (
-          <div>Loading...</div>
-        ) : (
-          <>
-            {brandSpecificDevice.map((device) => (
-              <Disclosure key={device.codename}>
-                {({ open }) => (
-                  <>
-                    <Disclosure.Button className='flex items-center justify-between w-full p-4 my-2 text-sm font-medium text-left text-gray-100 rounded-lg bg-[#332e4e] hover:bg-aex-200 focus:outline-none focus-visible:ring focus-visible:ring-purple-500 focus-visible:ring-opacity-75'>
-                      <span className='text-lg font-semibold '>{`${device.name} (${device.codename})`}</span>
-                      <div className='flex items-center  space-x-2  '>
-                        {device.supported_versions.map((supportedVersion) => (
-                          <p className='px-4 py-2 text-sm font-medium  text-white bg-emerald-400  rounded-r-full rounded-l-full shadow-sm'>
-                            {supportedVersion.version_name}
-                          </p>
-                        ))}
-                        <svg
-                          xmlns='http://www.w3.org/2000/svg'
-                          className={`${open ? 'transform rotate-180' : ''
-                            } w-5 h-5 text-purple-500`}
-                          fill='none'
-                          viewBox='0 0 24 24'
-                          stroke='currentColor'
-                        >
-                          <path
-                            strokeLinecap='round'
-                            strokeLinejoin='round'
-                            strokeWidth={2}
-                            d='M5 15l7-7 7 7'
-                          />
-                        </svg>
-                      </div>
-                    </Disclosure.Button>
-                    <Transition
-                      enter='transition duration-100 ease-out'
-                      enterFrom='transform scale-95 opacity-0'
-                      enterTo='transform scale-100 opacity-100'
-                      leave='transition duration-75 ease-out'
-                      leaveFrom='transform scale-100 opacity-100'
-                      leaveTo='transform scale-95 opacity-0'
-                    >
-                      <Disclosure.Panel className='w-full px-4 pt-4 pb-2 text-sm '>
-                        <div className='space-y-2 bg-aex-400 '>
-                          {device.supported_versions.map((supportedVersion) => (
-                            <Builds
-                              deviceBuilds={supportedVersion}
-                              codeName={device.codename}
-                              versionName={supportedVersion.version_name}
-                              maintainerName={supportedVersion.maintainer_name}
-                            />
-                          ))}
-                        </div>
-                      </Disclosure.Panel>
-                    </Transition>
-                  </>
-                )}
-              </Disclosure>
-            ))}
-          </>
-        )}
+        {brandSpecificDevice.map((device) => (
+          <Disclosure key={device.codename}>
+            {({ open }) => (
+              <>
+                <Disclosure.Button className='flex items-center justify-between w-full p-4 my-2  text-left text-gray-100 rounded-lg bg-[#332e4e] hover:bg-[#403960] focus:outline-none focus-visible:ring focus-visible:ring-aex-accent focus-visible:ring-opacity-75 transition-all'>
+                  <span className='font-medium'>{`${device.name} (${device.codename})`}</span>
+                  <div className='flex items-center space-x-2'>
+                    <div className='flex flex-row-reverse flex-wrap items-center gap-2 text-gray-100'>
+                      {device.supported_versions.map((supportedVersion) => (
+                        <p className='px-4 py-2 text-sm rounded-l-full rounded-r-full shadow-sm bg-emerald-400'>
+                          {supportedVersion.version_name}
+                        </p>
+                      ))}
+                    </div>
+                    <ChevronUpIcon className={`${open ? 'transform rotate-180' : ''
+                      } w-5 h-5 text-white`} />
+                  </div>
+                </Disclosure.Button>
+                <Transition
+                  enter='transition duration-100 ease-out'
+                  enterFrom='transform scale-95 opacity-0'
+                  enterTo='transform scale-100 opacity-100'
+                  leave='transition duration-75 ease-out'
+                  leaveFrom='transform scale-100 opacity-100'
+                  leaveTo='transform scale-95 opacity-0'
+                >
+                  <Disclosure.Panel className='w-full px-4 pt-4 pb-2 text-sm '>
+                    <div className='space-y-2 bg-aex-400 ' >
+                      {device.supported_versions.map((supportedVersion) => (
+                        <Builds
+                          deviceBuilds={supportedVersion}
+                          codeName={device.codename}
+                          versionName={supportedVersion.version_name}
+                          maintainerName={supportedVersion.maintainer_name}
+                        />
+                      ))}
+                    </div>
+                  </Disclosure.Panel>
+                </Transition>
+              </>
+            )}
+          </Disclosure>
+        ))}
       </h1>
     </div>
   )
@@ -196,13 +159,13 @@ export default function Devices({ brands, devices, initalDevices }) {
 
 export async function getStaticProps() {
   const response = await fetch(`https://api.aospextended.com/devices`)
-  const devices = await response.json()
-  const brands = getBrands(devices)
-
-  const initalDevices = devices.filter(
-    (device) => device.brand === brands[0]
+  const allSupportedDevice = await response.json()
+  const allSupportedBrand = getBrands(allSupportedDevice)
+  const initalDevices = allSupportedDevice.filter(
+    (device) => device.brand === allSupportedBrand[0]
   )
+
   return {
-    props: { brands, devices, initalDevices },
+    props: { allSupportedBrand, allSupportedDevice, initalDevices },
   }
 }
