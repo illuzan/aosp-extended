a874803a000f47a7919052e1dd203fd0f2a6c58f
Gaurav Singh <allstargaurav@gmail.com>
2022-05-27 14:42:49 +0530

    chore: update dependencies

--- next.config.js
+++ next.config.js
@@ -1,6 +1,9 @@
-module.exports = {
+/** @type {import('next').NextConfig} */
+const nextConfig = {
   reactStrictMode: true,
   images: {
     domains: ['github.com'],
   }
 }
+
+module.exports = nextConfig
