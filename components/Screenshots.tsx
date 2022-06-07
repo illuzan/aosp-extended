3ec0f7abb7ad21197a74a814b6ff875f75dd9e33
Gaurav Singh <allstargaurav@gmail.com>
2022-06-08 00:16:24 +0530

    feat: add new screenshot and post

--- components/Screenshots.tsx
+++ components/Screenshots.tsx
@@ -1,37 +1,137 @@
 import React from 'react'
-import { Swiper, SwiperSlide } from "swiper/react";
-import SwiperCore, { Pagination } from 'swiper';
+import { Swiper, SwiperSlide } from 'swiper/react'
+import SwiperCore, { Pagination } from 'swiper'
 
-SwiperCore.use([Pagination]);
+SwiperCore.use([Pagination])
 
 export default function Screenshot() {
   return (
-    <div className="max-w-xl px-4 mx-auto sm:px-6 lg:max-w-7xl lg:px-8">
-      <Swiper initialSlide={3} breakpoints={{
-        320: {
-          slidesPerView: 1,
-        },
-        920: {
-          slidesPerView: 2,
-        },
-        1024: {
-          slidesPerView: 3,
-        },
-      }}
-        spaceBetween={30} pagination={{
-          "clickable": true
-        }} className=" mySwiper">
-        <div className="swiper-pagination"></div>
-        <SwiperSlide><img className='p-12' src="screenshot/photo_2021-09-24_18-57-07.jpg" alt="" /></SwiperSlide>
-        <SwiperSlide><img className='p-12' src="screenshot/photo_2021-09-24_18-57-07.jpg" alt="" /></SwiperSlide>
-        <SwiperSlide><img className='p-12' src="screenshot/photo_2021-09-24_18-57-07.jpg" alt="" /></SwiperSlide>
-        <SwiperSlide><img className='p-12' src="screenshot/photo_2021-09-24_18-57-07.jpg" alt="" /></SwiperSlide>
-        <SwiperSlide><img className='p-12' src="screenshot/photo_2021-09-24_18-57-07.jpg" alt="" /></SwiperSlide>
-        <SwiperSlide><img className='p-12' src="screenshot/photo_2021-09-24_18-57-07.jpg" alt="" /></SwiperSlide>
-        <SwiperSlide><img className='p-12' src="screenshot/photo_2021-09-24_18-57-07.jpg" alt="" /></SwiperSlide>
-        <SwiperSlide><img className='p-12' src="screenshot/photo_2021-09-24_18-57-07.jpg" alt="" /></SwiperSlide>
-        <SwiperSlide><img className='p-12' src="screenshot/photo_2021-09-24_18-57-07.jpg" alt="" /></SwiperSlide>
-        <SwiperSlide><img className='p-12' src="screenshot/photo_2021-09-24_18-57-07.jpg" alt="" /></SwiperSlide>
+    <div className='max-w-xl px-4 mx-auto sm:px-6 lg:max-w-7xl lg:px-8'>
+      <Swiper
+        initialSlide={0}
+        breakpoints={{
+          320: {
+            slidesPerView: 1,
+          },
+          920: {
+            slidesPerView: 2,
+          },
+          1024: {
+            slidesPerView: 3,
+          },
+        }}
+        spaceBetween={12}
+        pagination={{
+          clickable: true,
+        }}
+        className=' mySwiper'
+      >
+        <div className='swiper-pagination'></div>
+        <SwiperSlide>
+          <img
+            className='p-12'
+            src='screenshot/gallery_1.webp'
+            alt='Screenshot of the OS'
+          />
+        </SwiperSlide>
+        <SwiperSlide>
+          <img
+            className='p-12'
+            src='screenshot/gallery_2.webp'
+            alt='Screenshot of the OS'
+          />
+        </SwiperSlide>
+        <SwiperSlide>
+          <img
+            className='p-12'
+            src='screenshot/gallery_3.webp'
+            alt='Screenshot of the OS'
+          />
+        </SwiperSlide>
+        <SwiperSlide>
+          <img
+            className='p-12'
+            src='screenshot/gallery_4.webp'
+            alt='Screenshot of the OS'
+          />
+        </SwiperSlide>
+        <SwiperSlide>
+          <img
+            className='p-12'
+            src='screenshot/gallery_5.webp'
+            alt='Screenshot of the OS'
+          />
+        </SwiperSlide>
+        <SwiperSlide>
+          <img
+            className='p-12'
+            src='screenshot/gallery_6.webp'
+            alt='Screenshot of the OS'
+          />
+        </SwiperSlide>
+        <SwiperSlide>
+          <img
+            className='p-12'
+            src='screenshot/gallery_7.webp'
+            alt='Screenshot of the OS'
+          />
+        </SwiperSlide>
+        <SwiperSlide>
+          <img
+            className='p-12'
+            src='screenshot/gallery_8.webp'
+            alt='Screenshot of the OS'
+          />
+        </SwiperSlide>
+        <SwiperSlide>
+          <img
+            className='p-12'
+            src='screenshot/gallery_9.webp'
+            alt='Screenshot of the OS'
+          />
+        </SwiperSlide>{' '}
+        <SwiperSlide>
+          <img
+            className='p-12'
+            src='screenshot/gallery_10.webp'
+            alt='Screenshot of the OS'
+          />
+        </SwiperSlide>
+        <SwiperSlide>
+          <img
+            className='p-12'
+            src='screenshot/gallery_11.webp'
+            alt='Screenshot of the OS'
+          />
+        </SwiperSlide>
+        <SwiperSlide>
+          <img
+            className='p-12'
+            src='screenshot/gallery_12.webp'
+            alt='Screenshot of the OS'
+          />
+        </SwiperSlide>
+        <SwiperSlide>
+          <img
+            className='p-12'
+            src='screenshot/gallery_13.webp'
+            alt='Screenshot of the OS'
+          />
+        </SwiperSlide>
+        <SwiperSlide>
+          <img
+            className='p-12'
+            src='screenshot/gallery_14.webp'
+            alt='Screenshot of the OS'
+          />
+        </SwiperSlide>
+        <SwiperSlide>
+          <img
+            className='p-12'
+            src='screenshot/gallery_15.webp'
+            alt='Screenshot of the OS'
+          />
+        </SwiperSlide>
       </Swiper>
     </div>
   )
---
+++ data/blog/2022-05-09-AospExtended-v9.1.md
@@ -0,0 +1,14 @@
+---
+title: AospExtended Twelve L v9.1 Official Release!
+author: Amol Amrit
+description: v9.1 May 2022 Release
+publishedAt: '2022-05-09'
+---
+
+- Initial release based on Android Twelve L with the May security patch!
+
+After a long break we are pushing official releases based on the latest android twelve L source. We hope you guys are doing good and are making the best use of your time. This release was a bit slower than our previous releases because all of us are a little busy at this point of time. We hope you guys respect our decision and we are all grateful for your support.
+
+Maintainers have been informed about the release and builds will be available for officially supported devices very soon. We have 3 new devices coming up this release, the OnePlus Nord 2 5G (denniz), Redmi 5 Plus (vince) and the Redmi Note 10 (mojito/sunny).
+
+Thanks, as always.
